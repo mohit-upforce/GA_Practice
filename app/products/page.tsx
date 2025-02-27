ba362060f3addefeb5639cf9d06db64bbfc8e7fc
vipul-upforce <mohit@upforce.tech>
2025-02-27 16:05:58 +0530

    .

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -12,6 +12,78 @@ const Products = () => {
                 <button>Buy</button>
             </footer>
         </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <span>$10.20 </span>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
     </div>
   )
 }
