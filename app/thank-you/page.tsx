c58a47b9eca221a80debeec55d7025ffa9d6a790
vipul-upforce <mohit@upforce.tech>
2025-02-27 16:02:10 +0530

    Meta Data Updated

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,5 +1,10 @@
+import { Metadata } from 'next';
 import React from 'react'
 
+export const metadata: Metadata = {
+    title: "Thank You Page",
+    description: "Thank You Page Desceription",
+  };
 const ThankYou = () => {
   return (
     <div>ThankYou</div>
