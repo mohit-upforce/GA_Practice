cfc6cda1f1851bf5a52a4226f9d0fa84e6fcde65
vipul-upforce <mohit@upforce.tech>
2025-02-27 15:45:11 +0530

    .

--- app/components/Utils/Navbar.tsx
+++ app/components/Utils/Navbar.tsx
@@ -6,9 +6,9 @@ const Navbar = () => {
     <div className='flex py-10'>
         <ul className='flex gap-5 mx-auto'>
             <li><Link href="/">Home</Link></li>
-            <li><Link href="/">Products</Link></li>
-            <li><Link href="/">Contact</Link></li>
-            <li><Link href="/">Cart</Link></li>
+            <li><Link href="/products">Products</Link></li>
+            <li><Link href="/contact">Contact</Link></li>
+            <li><Link href="/cart">Cart</Link></li>
             <li><Link href="/thank-you">Thank You</Link></li>
         </ul>
     </div>
---
+++ app/products/page.tsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const Products = () => {
+  return (
+    <div className='px-10 flex flex-col gap-5'>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>THis is a Products</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, quo. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores, voluptates libero a dolores et ab distinctio exercitationem maiores, mollitia consectetur inventore amet nam doloribus aspernatur</p>
+            <footer className='flex gap-4'>
+                <button>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+    </div>
+  )
+}
+
+export default Products
