09320a05c99eade6337f6fb0117ad87f9b791591
vipul-upforce <mohit@upforce.tech>
2025-02-27 14:46:34 +0530

    Google Tag Initialize

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ReactGA from "react-ga4";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +22,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  ReactGA.initialize("G-0XWDJ8PFMH");
 
   return (
     <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import ReactGA from "react-ga4";
 
 export default function Home() {
+
+  ReactGA.initialize("G-0XWDJ8PFMH");
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
