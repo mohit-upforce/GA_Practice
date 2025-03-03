269c1472dba690a7bfb65e548a41c7c8db002335
mohit-upforce <mohit@upforce.tech>
2025-03-03 17:49:48 +0530

    .

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import ReactGA from "react-ga4";
 
-const ProductCard = ({ title, description, price}: {title: string, description: string, price: number}) => {
+const ProductCard = ({ title, description, price }: { title: string, description: string, price: number }) => {
 
     const trackFooterClick = (label: string, url: string, price: number) => {
         console.log("Tracking click event:", label, url); // Debugging
@@ -15,17 +15,17 @@ const ProductCard = ({ title, description, price}: {title: string, description: 
             value: price, // URL of the clicked link
         });
     };
-  return (
-    <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
-                        <p>{title}</p>
-                        <p>{description}</p>
-                        <span>$ {price} </span>
-                        <footer className='flex gap-4'>
-                            <button onClick={() => trackFooterClick(title, description, price)}>Add to Cart</button>
-                            <button>Buy</button>
-                        </footer>
-                    </div>
-  )
+    return (
+        <div className='bg-[rgba(40,40,40,0.1)] rounded-md p-10'>
+            <p>{title}</p>
+            <p>{description}</p>
+            <span>$ {price} </span>
+            <footer className='flex gap-4'>
+                <button onClick={() => trackFooterClick(title, description, price)}>Add to Cart</button>
+                <button>Buy</button>
+            </footer>
+        </div>
+    )
 }
 
 export default ProductCard
