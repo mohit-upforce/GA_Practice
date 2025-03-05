efc9a5004bacb290b60fd511ad1450b88924e48b
mohit-upforce <mohit@upforce.tech>
2025-03-05 12:09:35 +0530

    .

--- app/components/Wrapper/Wrapper.tsx
+++ app/components/Wrapper/Wrapper.tsx
@@ -8,6 +8,7 @@ interface WrapperInterface{
 }
 const Wrapper:React.FC<WrapperInterface> = props => {
     ReactGA.initialize("G-0XWDJ8PFMH");
+    ReactGA.initialize("GTM-TN3L3KP4");
 
   return (
     <>{props.children}</>
