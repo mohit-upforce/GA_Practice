f42763f3833a7b3ba6772884c3c959d83eb555f8
mohit-upforce <mohit@upforce.tech>
2025-03-05 12:12:59 +0530

    Google Tag Manager addedq

--- app/components/Wrapper/Wrapper.tsx
+++ app/components/Wrapper/Wrapper.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactGA from "react-ga4";
-
+import TagManager from "react-gtm-module";  // Import GTM package
 interface WrapperInterface{
     children: React.ReactNode
 }
 const Wrapper:React.FC<WrapperInterface> = props => {
     ReactGA.initialize("G-0XWDJ8PFMH");
-    ReactGA.initialize("GTM-TN3L3KP4");
 
+    useEffect(() => {
+      TagManager.initialize({ gtmId: "GTM-TN3L3KP4" }); // Use GTM ID
+    }, []);
   return (
     <>{props.children}</>
   )
