6712755b6ce29b61a91bd73917d4f63e1acad8c1
mohit-upforce <mohit@upforce.tech>
2025-03-07 10:55:55 +0530

    .

--- app/components/Wrapper/Wrapper.tsx
+++ app/components/Wrapper/Wrapper.tsx
@@ -9,9 +9,9 @@ interface WrapperInterface{
 const Wrapper:React.FC<WrapperInterface> = props => {
     ReactGA.initialize("G-0XWDJ8PFMH");
 
-    useEffect(() => {
-      TagManager.initialize({ gtmId: "GTM-TN3L3KP4" }); // Use GTM ID
-    }, []);
+    // useEffect(() => {
+    //   TagManager.initialize({ gtmId: "GTM-TN3L3KP4" }); // Use GTM ID
+    // }, []);
   return (
     <>{props.children}</>
   )
